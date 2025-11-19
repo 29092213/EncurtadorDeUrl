67d00f72f5580689af151f1ee499540306226dfd
Luiz Souza <luiz.souza@ismdsp.local>
2025-11-19 17:27:56 -0300

    create Controller

--- src/main/java/com/encurtadorUrl/Encurtador/CONTROLLER/UrlController.java
+++ src/main/java/com/encurtadorUrl/Encurtador/CONTROLLER/UrlController.java
@@ -6,11 +6,12 @@ import com.encurtadorUrl.Encurtador.ENTITY.UrlEntity;
 import com.encurtadorUrl.Encurtador.REPOSITORIE.UrlRepositorie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 
 @RestController
@@ -36,10 +37,24 @@ public class UrlController {
                 .replace("/createUrl", "/" + id);
 
         urlRepositorie.save(new UrlEntity(id, dto.url(), newUrlReduce, LocalDateTime.now().plusMinutes(2)));
+
         return ResponseEntity.ok(new UrlResponse(newUrlReduce));
 
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Void> redirectEndpoint(@PathVariable("id") String id){
 
+        var url = urlRepositorie.findById(id);
 
+        if (url.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setLocation(URI.create(url.get().getFullUrl()));
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
 }
